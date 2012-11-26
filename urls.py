108b2a3fc1f7052d16737cf72d6f00d1423aae5b
Aapo Rista <arista@ilmaloota>
2012-11-26 17:20:08 +0200

    Added new view, which displays preview field's content. Original() can now have filename after uid.

--- urls.py
+++ urls.py
@@ -12,9 +12,11 @@ urlpatterns = patterns('',
     url(r'^html5upload/$', views.html5upload, name='html5upload'),
     url(r'^edit/(?P<uid>[\w]+)/$', views.edit, name='edit'),
     url(r'^instance/(?P<uid>[\w]+)-(?P<width>\d+)x(?P<height>\d+)(?P<action>\-\w+)?\.(?P<ext>\w+)$', views.instance, name='instance'),
+    url(r'^view/(?P<uid>[\w]+)-(?P<width>(\d+|%d))x(?P<height>(\d+|%d))(?P<action>\-\w+)?\.(?P<ext>\w+)$', views.view, name='view'),
     url(r'^foobar/(?P<uid>[\w]+)-(?P<id>[\d]+)\.(?P<ext>\w+)$', views.foobar, name='foobar'),
-    (r'^original/([\w+\.]+)$', views.original),
-    (r'^metadata/(\w+)$', views.metadata),
+    url(r'^original/(?P<uid>[\w]+)/(?P<filename>.+)$', views.original, name='original'),
+    url(r'^original/(?P<uid>[\w]+)$', views.original, name='original'),
+    url(r'^metadata/(\w+)$', views.metadata, name='metadata'),
 )
 
 
