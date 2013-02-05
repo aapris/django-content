55290775d70c651273c7270d3717144b84af2464
Aapo Rista <arista@ilmaloota>
2013-02-05 10:50:00 +0200

    Trying to avoid some wsgi issue and changed Task class to @task decorator.

--- tasks.py
+++ tasks.py
@@ -1,16 +1,16 @@
 from celery.registry import tasks
 from celery.task import Task 
-#from models import Trackpost
+from celery import task
+from django.core import management
 
+# This is having some problems with celery 3.0.11 and mod_wsgi
 class CreateInstancesTask(Task):
-    #def run(self, **kwargs):
-    #name = 'CheckWebsiteTask'
     def run(self, pk):
-        #tp = Trackpost.objects.get(pk=pk)
-        #print tp.file.path
-        from django.core import management
         management.call_command('create_instances', verbosity=0, pk=pk)
-        #management.call_command('loaddata', 'test_data', verbosity=0)
 
 tasks.register(CreateInstancesTask)
 
+
+@task()
+def create_instances_task(pk):
+    management.call_command('create_instances', verbosity=0, pk=pk)
