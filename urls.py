f04749278a497cfe33ec21ea0a5af98f62347762
Aapo (f021b) Rista <aapris@gmail.com>
2022-04-21 20:51:58 +0300

    Add content.urls

---
+++ urls.py
@@ -0,0 +1,15 @@
+"""
+Provide urlpatterns for original content file, preview and instances.
+"""
+from django.urls import path, re_path
+
+from content import views
+
+urlpatterns = [
+    path("original/<str:uid>/<str:filename>", views.original, name="original"),
+    re_path(
+        r"preview/(?P<uid>\w+)-(?P<width>(\d+|%d))x(?P<height>(\d+|%d))(?P<action>-\w+)?\.(?P<ext>\w+)$",
+        views.preview,
+        name="preview",
+    ),
+]
