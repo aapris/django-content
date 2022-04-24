588186ef04373b5e21b008088afc5e0b94849b10
Aapo (f021b) Rista <aapris@gmail.com>
2022-04-24 14:08:34 +0300

    Remove py2 stuff

--- tasks.py
+++ tasks.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from celery.registry import tasks
-from celery.task import Task 
+from celery.task import Task
 from celery import task
 from django.core import management
+
 
 # This is having some problems with celery 3.0.13 and mod_wsgi
 class CreateInstancesTask(Task):
     def run(self, pk):
-        management.call_command('create_instances', verbosity=0, pk=pk)
+        management.call_command("create_instances", verbosity=0, pk=pk)
+
 
 tasks.register(CreateInstancesTask)
 
 
 @task()
 def create_instances_task(pk):
-    management.call_command('create_instances', verbosity=0, pk=pk)
+    management.call_command("create_instances", verbosity=0, pk=pk)
